73db01c9b72d30eab9999135a29594e8a645adbe
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-13 21:33:33 +0500

    Add src/entities/Arrival.ts

---
+++ src/entities/Arrival.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('arrivals')
+export class Arrival {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name!: string;
+
+  @Column({ type: 'varchar', length: 50, default: 'pending' })
+  status!: string;
+
+  @Column({ type: 'int', default: 0 })
+  quantity!: number;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
